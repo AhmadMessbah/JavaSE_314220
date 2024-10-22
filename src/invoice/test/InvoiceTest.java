6190c6ff129bea61838f5b273406c7fca6f0e60f
Farshid Rezaiyan <farshidrezaiyan@gmail.com>
2024-10-22 20:16:45 +0330

    farshid rezaiyan Invoice

---
+++ src/invoice/test/InvoiceTest.java
@@ -0,0 +1,14 @@
+package invoice.test;
+
+import invoice.model.entity.Product;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InvoiceTest {
+    public static void main(String[] args) throws Exception {
+    }
+}
