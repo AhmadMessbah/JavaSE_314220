f829a19a96490f34c2ccc00d3aa1ce8b9f5f73d5
Messbah.A <Messbah.A@Gmail.com>
2024-10-22 20:24:09 +0330

    invoice updated - mvc - messbah.a

--- src/mft/controller/InvoiceController.java
+++ src/mft/controller/InvoiceController.java
@@ -1,13 +1,22 @@
-package mft.controller;
+package invoice.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import invoice.model.entity.Invoice;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class InvoiceController implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    private static int id = 0;
+    private List<Invoice> invoicetList = new ArrayList<>();
 
-    }
+
 }
