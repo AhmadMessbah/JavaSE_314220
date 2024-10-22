4ab896561af6285c18108a2f15de0033d3edca4e
Messbah.A <Messbah.A@Gmail.com>
2024-10-22 19:36:13 +0330

    product updated - mvc - messbah.a

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-Salam
+Tasks :
 
-salam aleik
+    - oveisi    : Customer(id, name, family, username, password, phone, active)
+    - bakhshi   : Branch(id, title, address, phone, area, active)
+    - pasdar    : Contact(id, state, city, region, address,postalCode, phone, description)
+    - jalilian  : Payment(id, title, amount, dateTime, description, paymentType(Check,Cash,Card))
+    - aligholi  : Delivery(id, productName, address, dateTime, deliveryStatus(NotReady, Ready, Sent, Delivered))
+    - rezaziian : Invoice(id, invoiceSerial, productName, quantity, price, dateTime, customerName)
+
+Order :
+
+    - model/entity
+    - test
+    - model/repository
+    - model/service
+    - test
+    - controller
+    - view
+    - test
--- src/mft/test/ProductTest.java
+++ src/mft/test/ProductTest.java
@@ -1,45 +1,51 @@
 package mft.test;
 
+import mft.model.entity.Product;
+import mft.model.entity.enums.Category;
+import mft.model.entity.enums.TransactionType;
 import mft.model.service.ProductService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductTest {
     public static void main(String[] args) throws Exception {
         System.out.println(ProductService.findAll());
 
-//        Product product1 =
-//                Product
-//                        .builder()
-//                        .id(1)
-//                        .name("Asus 1230")
-//                        .price(1800)
-//                        .quantity(3)
-//                        .discount(0)
-//                        .category(Category.Electrical)
-//                        .transactionType(TransactionType.outcome)
-//                        .expireDate(LocalDate.now())
-//                        .image(false)
-//                        .catalogue(true)
-//                        .build();
-//
-//
-//        String line = "2,mobile,7000,3,Electrical,2024-12-15,0,true,true,outcome";
-//        Product product2 = new Product(line);
-//
-//
-//        System.out.println(product1);
-//        System.out.println(product2);
-//
-//        List<Product> productList = new ArrayList<>();
-//        productList.add(product1);
-//        productList.add(product2);
-//        ProductRepository repo = new ProductRepository();
-//        repo.saveAll(productList);
-//        System.out.println(repo.findAll());
-//
-//        System.out.println(product);
-//
-//        System.out.println(product.toCsv());
+        Product product1 =
+                Product
+                        .builder()
+                        .id(1)
+                        .name("Asus 1230")
+                        .price(1800)
+                        .quantity(3)
+                        .discount(0)
+                        .category(Category.Electrical)
+                        .transactionType(TransactionType.outcome)
+                        .expireDate(LocalDate.now())
+                        .image(false)
+                        .catalogue(true)
+                        .build();
+
+
+        String line = "2,mobile,7000,3,Electrical,2024-12-15,0,true,true,outcome";
+        Product product2 = new Product(line);
+
+
+        System.out.println(product1);
+        System.out.println(product2);
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(product1);
+        productList.add(product2);
+
+        ProductService.saveAll(productList);
+        System.out.println(ProductService.findAll());
+
+        System.out.println(product1);
+        System.out.println(product1.toCsv());
 
     }
 }
