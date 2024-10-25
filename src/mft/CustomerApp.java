7b632d7224e4d269d0d606aa0cacb810483e7bf0
Babak Oveisi <babakoveisi1375@gmail.com>
2024-10-25 11:25:44 +0330

    update CustomerApp.java - oveisi

---
+++ src/mft/CustomerApp.java
@@ -0,0 +1,24 @@
+package mft;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CustomerApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(
+                FXMLLoader.load(getClass().getResource("veiw/customerForm.fxml"))
+        );
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Customer Form");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main");
+        launch();
+    }
+}
