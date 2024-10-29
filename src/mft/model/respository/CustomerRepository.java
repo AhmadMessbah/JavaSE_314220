65da8c57c2051b28d7bbc744c63ce4c0b7ff66a0
Babak Oveisi <babakoveisi1375@gmail.com>
2024-10-29 20:31:44 +0330

    update CustomerRepository.java - Babak Oveisi

--- src/mft/model/respository/CustomerRepository.java
+++ src/mft/model/respository/CustomerRepository.java
@@ -5,50 +5,42 @@ import mft.model.entity.Customer;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CustomerRepository {
+public class CustomerRepository implements AutoCloseable{
     private Connection connection;
-
-    public void connect() throws SQLException {
-        connection =
-                DriverManager.getConnection(
-                        "jdbc:oracle:thin:@localhost:1521:xe",
-                        "javase",
-                        "java123"
-                );
-    }
-
-    public void disconnect() throws SQLException {
-        connection.close();
-    }
+    private PreparedStatement preparedStatement;
 
     // Transactions:
     public void save(Customer customer) throws SQLException {
-        connect();
+        connection = ConnectionProvider.getConnection();
+        preparedStatement =
+                connection.prepareStatement("SELECT customer_seq.nextval AS next_id FROM dual");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        customer.setId(resultSet.getInt("next_id"));
+
         PreparedStatement preparedStatement =
                 connection.prepareStatement(
                         "INSERT INTO customers" +
                                 " (id, name, familyName, username, password, phone, active)" +
-                                " VALUES (customer_seq.nextval, ?, ?, ?, ?, ?, ?)"
+                                " VALUES (?, ?, ?, ?, ?, ?, ?)"
                 );
-        preparedStatement.setString(1, customer.getName());
-        preparedStatement.setString(2, customer.getFamilyName());
-        preparedStatement.setString(3, customer.getUsername());
-        preparedStatement.setString(4, customer.getPassword());
-        preparedStatement.setString(5, customer.getPhone());
-        preparedStatement.setBoolean(6, customer.isActive());
-        disconnect();
+        preparedStatement.setInt(1, customer.getId());
+        preparedStatement.setString(2, customer.getName());
+        preparedStatement.setString(3, customer.getFamilyName());
+        preparedStatement.setString(4, customer.getUsername());
+        preparedStatement.setString(5, customer.getPassword());
+        preparedStatement.setString(6, customer.getPhone());
+        preparedStatement.setBoolean(7, customer.isActive());
+        preparedStatement.execute();
     }
 
     public void edit(Customer customer) throws SQLException {
-        connect();
+        connection = ConnectionProvider.getConnection();
 
         PreparedStatement preparedStatement =
                 connection.prepareStatement(
@@ -64,20 +56,101 @@ public class CustomerRepository {
         preparedStatement.setString(5, customer.getPhone());
         preparedStatement.setBoolean(6, customer.isActive());
         preparedStatement.setInt(7, customer.getId());
-
-        disconnect();
+        preparedStatement.execute();
     }
 
-    public void remove(Customer customer) throws SQLException {
-        connect();
+    public void remove(int id) throws SQLException {
+        connection = ConnectionProvider.getConnection();
 
         PreparedStatement preparedStatement =
                 connection.prepareStatement(
                         "DELETE FROM customers WHERE id=?"
                 );
 
-        preparedStatement.setInt(1, customer.getId());
+        preparedStatement.setInt(1, id);
+        preparedStatement.execute();
+    }
 
-        disconnect();
+    public List<Customer> findAll() throws SQLException {
+        connection = ConnectionProvider.getConnection();
+        preparedStatement =
+                connection.prepareStatement("SELECT * FROM customers ORDER BY name");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<Customer> customerList = new ArrayList<>();
+        while (resultSet.next()) {
+            Customer customer =
+                    Customer
+                            .builder()
+                            .id(resultSet.getInt("id"))
+                            .name(resultSet.getString("name"))
+                            .familyName(resultSet.getString("familyName"))
+                            .username(resultSet.getString("username"))
+                            .password(resultSet.getString("password"))
+                            .phone(resultSet.getString("phone"))
+                            .active(resultSet.getBoolean("active"))
+                            .build();
+            customerList.add(customer);
+        }
+
+        return customerList;
+    }
+
+    public Customer findById(int id) throws SQLException {
+        connection = ConnectionProvider.getConnection();
+        preparedStatement =
+                connection.prepareStatement("SELECT * FROM customers WHERE id=?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Customer customer = null;
+        if (resultSet.next()) {
+            customer =
+                    Customer
+                            .builder()
+                            .id(resultSet.getInt("id"))
+                            .name(resultSet.getString("name"))
+                            .familyName(resultSet.getString("familyName"))
+                            .username(resultSet.getString("username"))
+                            .password(resultSet.getString("password"))
+                            .phone(resultSet.getString("phone"))
+                            .active(resultSet.getBoolean("active"))
+                            .build();
+        }
+
+        return customer;
+    }
+
+    public List<Customer> findByName(String name) throws SQLException {
+        connection = ConnectionProvider.getConnection();
+        preparedStatement =
+                connection.prepareStatement("SELECT * FROM customers WHERE name LIKE ?");
+        preparedStatement.setString(1, name);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<Customer> customerList = new ArrayList<>();
+        while (resultSet.next()) {
+            Customer customer =
+                    Customer
+                            .builder()
+                            .id(resultSet.getInt("id"))
+                            .name(resultSet.getString("name"))
+                            .familyName(resultSet.getString("familyName"))
+                            .username(resultSet.getString("username"))
+                            .password(resultSet.getString("password"))
+                            .phone(resultSet.getString("phone"))
+                            .active(resultSet.getBoolean("active"))
+                            .build();
+
+            customerList.add(customer);
+        }
+
+        return customerList;
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
     }
 }
