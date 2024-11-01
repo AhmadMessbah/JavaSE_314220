c013e74d5ef0fb62946bf556165c1cc9b9f848f3
irBabak <babakoveisi1375@gmail.com>
2024-11-01 17:58:49 +0330

    update CustomerService - Babak Oveisi

--- src/mft/model/service/CustomerService.java
+++ src/mft/model/service/CustomerService.java
@@ -3,20 +3,43 @@ package mft.model.service;
 import mft.model.entity.Customer;
 import mft.model.respository.CustomerRepository;
 
+import java.util.List;
+
 //Bussiness Logic:
 public class CustomerService {
     public static void save(Customer customer) throws Exception {
-        CustomerRepository repository = new CustomerRepository();
-        repository.save(customer);
+        try (CustomerRepository repository = new CustomerRepository()) {
+            repository.save(customer);
+        }
     }
 
     public static void edit(Customer customer) throws Exception {
-        CustomerRepository repository = new CustomerRepository();
-        repository.edit(customer);
+        try (CustomerRepository repository = new CustomerRepository()) {
+            repository.edit(customer);
+        }
     }
 
-    public static void remove(Customer customer) throws Exception {
-        CustomerRepository repository = new CustomerRepository();
-        repository.remove(customer);
+    public static void remove(int id) throws Exception {
+        try (CustomerRepository repository = new CustomerRepository()) {
+            repository.remove(id);
+        }
+    }
+
+    public static List<Customer> findAll() throws Exception {
+        try (CustomerRepository repository = new CustomerRepository()) {
+            return repository.findAll();
+        }
+    }
+
+    public static Customer findById(int id) throws Exception {
+        try (CustomerRepository repository = new CustomerRepository()) {
+            return repository.findById(id);
+        }
+    }
+
+    public static List<Customer> findByName(String name) throws Exception {
+        try (CustomerRepository repository = new CustomerRepository()) {
+            return repository.findByName(name);
+        }
     }
 }
